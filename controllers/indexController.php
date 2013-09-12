07463bde916625313b0668475d89be091efdcf8f
cclehui <god_chen@sina.com>
2013-09-12 15:20:05 +0800

    init

---
+++ controllers/indexController.php
@@ -0,0 +1,16 @@
+<?php
+	class indexController extends baseController{
+		
+		public function __construct(){
+			parent::__construct();
+		}
+		public function index(){
+
+				$this->load->model('posts');
+
+				$vars['title'] = 'Dynamic title';
+				$vars['posts'] = $this->posts->getEntries();
+				$this->load->view('index',$vars);	
+		}
+
+	}
